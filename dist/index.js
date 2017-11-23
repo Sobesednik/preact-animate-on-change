81d535d4431765e49f652baf341d18a95ec77134
Jason Miller <jasonmiller@synacor.com>
2017-11-22 19:15:13 -0500

    [dist]

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,8 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+exports.__esModule = true;
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _propTypes = require('prop-types');
-
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _preact = require('preact');
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -45,109 +33,103 @@ var AnimateOnChange = function (_Component) {
   function AnimateOnChange(props) {
     _classCallCheck(this, AnimateOnChange);
 
-    var _this = _possibleConstructorReturn(this, (AnimateOnChange.__proto__ || Object.getPrototypeOf(AnimateOnChange)).call(this, props));
+    var _this = _possibleConstructorReturn(this, _Component.call(this, props));
 
     _this.state = { animating: false, clearAnimationClass: false };
+    _this.rootRef = _this.rootRef.bind(_this);
     _this.animationStart = _this.animationStart.bind(_this);
     _this.animationEnd = _this.animationEnd.bind(_this);
     return _this;
   }
 
-  _createClass(AnimateOnChange, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      var elm = this.refs.root;
-      this.addEventListener('start', elm, this.animationStart);
-      this.addEventListener('end', elm, this.animationEnd);
-    }
-  }, {
-    key: 'componentWillUnmount',
-    value: function componentWillUnmount() {
-      var elm = this.refs.root;
-      this.removeEventListeners('start', elm, this.animationStart);
-      this.removeEventListeners('end', elm, this.animationEnd);
-    }
-  }, {
-    key: 'addEventListener',
-    value: function addEventListener(type, elm, eventHandler) {
-      // until an event has been triggered bind them all
-      events[type].map(function (event) {
-        // console.log(`adding ${event}`)
-        elm.addEventListener(event, eventHandler);
-      });
-    }
-  }, {
-    key: 'removeEventListeners',
-    value: function removeEventListeners(type, elm, eventHandler) {
-      events[type].map(function (event) {
-        // console.log(`removing ${event}`)
-        elm.removeEventListener(event, eventHandler);
-      });
-    }
-  }, {
-    key: 'updateEvents',
-    value: function updateEvents(type, newEvent) {
-      // console.log(`updating ${type} event to ${newEvent}`)
-      events[type + 'Removed'] = events[type].filter(function (e) {
-        return e !== newEvent;
-      });
-      events[type] = [newEvent];
-    }
-  }, {
-    key: 'animationStart',
-    value: function animationStart(e) {
-      if (events['start'].length > 1) {
-        this.updateEvents('start', e.type);
-        this.removeEventListeners('startRemoved', this.refs.root, this.animationStart);
-      }
-      this.setState({ animating: true, clearAnimationClass: false });
-    }
-  }, {
-    key: 'animationEnd',
-    value: function animationEnd(e) {
-      if (events['end'].length > 1) {
-        this.updateEvents('end', e.type);
-        this.removeEventListeners('endRemoved', this.refs.root, this.animationStart);
-      }
-      // send separate, animation state change will not render
-      this.setState({ clearAnimationClass: true }); // renders
-      this.forceUpdate();
-      this.setState({ animating: false, clearAnimationClass: false });
-    }
-  }, {
-    key: 'shouldComponentUpdate',
-    value: function shouldComponentUpdate(nextProps, nextState) {
-      if (this.state.animating !== nextState.animating) {
-        // do not render on animation change
-        return false;
-      }
-      return true;
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      var className = this.props.baseClassName;
+  AnimateOnChange.prototype.rootRef = function rootRef(ref) {
+    this.root = ref;
+  };
 
-      if (this.props.animate && !this.state.clearAnimationClass) {
-        className += ' ' + this.props.animationClassName;
-      }
+  AnimateOnChange.prototype.componentDidMount = function componentDidMount() {
+    var elm = this.root;
+    this.addEventListener('start', elm, this.animationStart);
+    this.addEventListener('end', elm, this.animationEnd);
+  };
 
-      return _react2.default.createElement(
-        'span',
-        { ref: 'root', className: className },
-        this.props.children
-      );
+  AnimateOnChange.prototype.componentWillUnmount = function componentWillUnmount() {
+    var elm = this.root;
+    this.removeEventListeners('start', elm, this.animationStart);
+    this.removeEventListeners('end', elm, this.animationEnd);
+  };
+
+  AnimateOnChange.prototype.addEventListener = function addEventListener(type, elm, eventHandler) {
+    // until an event has been triggered bind them all
+    events[type].map(function (event) {
+      // console.log(`adding ${event}`)
+      elm.addEventListener(event, eventHandler);
+    });
+  };
+
+  AnimateOnChange.prototype.removeEventListeners = function removeEventListeners(type, elm, eventHandler) {
+    events[type].map(function (event) {
+      // console.log(`removing ${event}`)
+      elm.removeEventListener(event, eventHandler);
+    });
+  };
+
+  AnimateOnChange.prototype.updateEvents = function updateEvents(type, newEvent) {
+    // console.log(`updating ${type} event to ${newEvent}`)
+    events[type + 'Removed'] = events[type].filter(function (e) {
+      return e !== newEvent;
+    });
+    events[type] = [newEvent];
+  };
+
+  AnimateOnChange.prototype.animationStart = function animationStart(e) {
+    if (events['start'].length > 1) {
+      this.updateEvents('start', e.type);
+      this.removeEventListeners('startRemoved', this.root, this.animationStart);
     }
-  }]);
+    this.setState({ animating: true, clearAnimationClass: false });
+  };
+
+  AnimateOnChange.prototype.animationEnd = function animationEnd(e) {
+    var _this2 = this;
+
+    if (events['end'].length > 1) {
+      this.updateEvents('end', e.type);
+      this.removeEventListeners('endRemoved', this.root, this.animationStart);
+    }
+    // send separate, animation state change will not render
+    this.setState({ clearAnimationClass: true }, function () {
+      _this2.setState({ animating: false, clearAnimationClass: false });
+    });
+  };
+
+  AnimateOnChange.prototype.shouldComponentUpdate = function shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.animating !== nextState.animating) {
+      // do not render on animation change
+      return false;
+    }
+    return true;
+  };
+
+  AnimateOnChange.prototype.render = function render(_ref, _ref2) {
+    var className = _ref.baseClassName,
+        animate = _ref.animate,
+        animationClassName = _ref.animationClassName,
+        children = _ref.children;
+    var clearAnimationClass = _ref2.clearAnimationClass;
+
+    if (animate && !clearAnimationClass) {
+      className += ' ' + animationClassName;
+    }
+
+    return (0, _preact.h)(
+      'span',
+      { ref: this.rootRef, className: className },
+      children
+    );
+  };
 
   return AnimateOnChange;
-}(_react.Component);
-
-AnimateOnChange.propTypes = {
-  children: _propTypes2.default.any.isRequired,
-  animate: _propTypes2.default.bool.isRequired,
-  baseClassName: _propTypes2.default.string.isRequired,
-  animationClassName: _propTypes2.default.string.isRequired
-};
+}(_preact.Component);
 
 exports.default = AnimateOnChange;
+module.exports = exports['default'];
