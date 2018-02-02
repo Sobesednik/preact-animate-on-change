f5a7bfe149cdaf739002f0ab8134a4ddd5b2b174
Anton <anton@sobesednik.media>
2018-02-02 15:26:09 +0000

    setup ci

---
+++ upgrade.js
@@ -0,0 +1,15 @@
+const spawncommand = require('spawncommand')
+const { devDependencies, dependencies } = require('./package.json');
+
+(async () => {
+	const keys = Object.keys({ ...devDependencies, ...dependencies })
+		.map(s => `${s}@latest`)
+
+	const proc = spawncommand('yarn', ['upgrade', ...keys])
+	console.log(proc.spawnCommand)
+
+	proc.stdout.pipe(process.stdout)
+	proc.stderr.pipe(process.stderr)
+
+	const { stdout, stderr, code } = await proc.promise
+})()
